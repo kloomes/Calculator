1c707cad36f5791d752e98a597a2bab4a77e133d
Karl Loomes <kloomes@hotmail.com>
2020-09-24 16:59:11 +0100

    All functionality and repeat

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -6,25 +6,72 @@ public class Calculator
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        options();
+    }
+
+    public static void options() {
+      Scanner input = new Scanner(System.in);
         System.out.println("What would you like to do?");
         System.out.println("1) Add");
         System.out.println("2) Subtract");
         System.out.println("3) Multiply");
         System.out.println("4) Divide");
+        System.out.println("5) Exit");
         switch (input.nextInt()) {
           case 1:
           System.out.println("The answer is " + add(getNum(), getNum()));
+          goAgain();
+          break;
+          case 2:
+          System.out.println("The answer is " + subtract(getNum(), getNum()));
+          goAgain();
+          break;
+          case 3:
+          System.out.println("The answer is " + multiply(getNum(), getNum()));
+          goAgain();
+          break;
+          case 4:
+          System.out.println("The answer is " + divide(getNum(), getNum()));
+          goAgain();
+          break;
+          case 5:
+          break;
         }
     }
 
-    public static int getNum() {
+    public static double getNum() {
       Scanner input = new Scanner(System.in);
       System.out.println("What number?");
-      return input.nextInt();
+      return input.nextDouble();
     }
 
-    public static int add(int x, int y) {
+    public static void goAgain() {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Go again? y/n");
+      String answer = input.nextLine();
+      if (answer.equals("n")) {
+        System.out.println("Exiting...");
+      } else if (answer.equals("y")) {
+        options();
+      } else {
+        System.out.println("y or n only please.");
+        goAgain();
+      }
+    }
+
+    public static double add(double x, double y) {
       return x + y;
+    }
+
+    public static double subtract(double x, double y) {
+      return x - y;
+    }
+
+    public static double multiply(double x, double y) {
+      return x * y;
+    }
+
+    public static double divide(double x, double y) {
+      return x / y;
     }
 }
